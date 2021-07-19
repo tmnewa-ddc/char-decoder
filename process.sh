9867cee3f8481b58988b2c80b62f20e993a6aa65
chris <x74426000@gmail.com>
2021-07-19 17:08:39 +0800

    [update] bash script

--- process.sh
+++ process.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-
 mode=${1:-0}
 
-
-
 for file in file/source/*
-do  
-  if [ ! -f file ]; then
+do
+  if [ ! -f $file ]; then
       echo "there is no file..."
       break
   fi
+  rm file/target/${file##*/}
 
   if [[ $mode -eq '0' ]]; then
     echo 'use iconv (bash)'
-    iconv -f BIG5-HKSCS:2001 -t UTF8 $file > file/target/${file##*/}
+    iconv -f BIG5HKSCS -t UTF8 $file > file/target/${file##*/}
     mv $file file/backup/
   else
     echo 'use iconv-lite (vscode)'
