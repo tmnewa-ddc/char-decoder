ac1e031e928f9de65e86d94bd8e42170c5859bdb
chris <x74426000@gmail.com>
2021-07-19 17:47:46 +0800

    [update] process while iconv error from shell

--- process.sh
+++ process.sh
@@ -11,9 +11,16 @@ do
   rm file/target/${file##*/}
 
   if [[ $mode -eq '0' ]]; then
-    echo 'use iconv (bash)'
-    iconv -f BIG5HKSCS -t UTF8 $file > file/target/${file##*/}
-    mv $file file/backup/
+    ir=1
+    { # try
+      iconv -f BIG5HKSCS -t UTF8 $file > file/target/${file##*/}
+    } || {
+      ir=0
+      rm file/target/${file##*/}
+    }
+    if [[ $ir -eq 1 ]]; then
+      mv $file file/backup/
+    fi
   else
     echo 'use iconv-lite (vscode)'
     npm run big5 $file
